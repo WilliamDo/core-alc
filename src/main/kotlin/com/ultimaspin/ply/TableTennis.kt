78ef09af8d40f5c15dfabc198f4e47af599d2ecb
William Do <william@ultimaspin.com>
2019-04-17 18:32:21 +0100

    move game class into a separate file

--- src/main/kotlin/com/ultimaspin/ply/TableTennis.kt
+++ src/main/kotlin/com/ultimaspin/ply/TableTennis.kt
@@ -1,7 +1,5 @@
 package com.ultimaspin.ply
 
-import java.lang.IllegalStateException
-
 class Player(val name: String)
 
 class Match(val player1: Player,
@@ -27,56 +25,3 @@ class Match(val player1: Player,
 
 }
 
-class Game(val player1: Player,
-           val pointsForPlayer1: Int,
-           val player2: Player,
-           val pointsForPlayer2: Int,
-           private val pointsToWin: Int = DEFAULT_POINTS_TO_WIN) {
-
-    init {
-        if (!(isPlayer1TheWinner() xor isPlayer2TheWinner())) {
-            throw IllegalStateException("Invalid game state")
-        }
-    }
-
-
-    fun getWinner(): Player {
-
-        if (isPlayer1TheWinner()) {
-            return player1
-        } else if (isPlayer2TheWinner()) {
-            return player2
-        }
-
-        throw IllegalStateException("Cannot determine winner of this game")
-
-    }
-
-    private fun isPlayer1TheWinner(): Boolean {
-        val player1WinsCleanGame = pointsForPlayer1 == pointsToWin && pointsForPlayer2 <= pointsToWin - 2
-        val player1WinsAfterDeuce = pointsForPlayer1 > pointsToWin && pointsForPlayer1 - pointsForPlayer2 >= 2
-        return player1WinsCleanGame || player1WinsAfterDeuce
-    }
-
-    private fun isPlayer2TheWinner(): Boolean {
-        val player2WinsCleanGame = pointsForPlayer2 == pointsToWin && pointsForPlayer1 <= pointsToWin - 2
-        val player2WinsAfterDeuce = pointsForPlayer2 > pointsToWin && pointsForPlayer2 - pointsForPlayer1 >= 2
-        return player2WinsCleanGame || player2WinsAfterDeuce
-    }
-
-
-    fun getLoser(): Player {
-        if (isPlayer1TheWinner()) {
-            return player2
-        } else if (isPlayer2TheWinner()) {
-            return player1
-        }
-
-        throw IllegalStateException("Cannot determine lost of this game")
-    }
-
-    companion object {
-        const val DEFAULT_POINTS_TO_WIN: Int = 11
-    }
-
-}
